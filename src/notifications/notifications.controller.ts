d5ed2d22b4fa447708bb28f9320ef43f3e569698
hoseok <hs1j333@gmail.com>
2025-07-09 12:59:54 +0900

    commit - 20250709 sse 알람 구현 변경

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Inject, Logger, Sse, UseGuards } from '@nestjs/common';
+import { Controller, Inject, Sse, UseGuards } from '@nestjs/common';
 import { SseJwtAuthGuard } from "../auth";
 import { AuthQuerySchema, User } from "../common";
-import { from, Observable, tap } from "rxjs";
+import { Observable } from "rxjs";
 import { ApiOkResponse, ApiOperation, ApiQuery, ApiTags } from "@nestjs/swagger";
 import { NotificationSchema } from "./api";
 import { NotificationsService } from "./notifications.service";
@@ -10,24 +10,19 @@ import { NotificationsService } from "./notifications.service";
 @Controller('/api/notifications')
 @UseGuards(SseJwtAuthGuard)
 export class NotificationsController {
-    private readonly _logger: Logger = new Logger(NotificationsController.name);
 
     constructor(
         @Inject(NotificationsService)
         private readonly _notificationsService: NotificationsService,
     ) {}
 
-    @Sse("/")
+    @Sse()
     @ApiOperation({ summary: "sse endpoint" })
     @ApiQuery({ type: AuthQuerySchema, required: true })
     @ApiOkResponse({ type: NotificationSchema })
     notifyUser(@User("id") userId: number): Observable<NotificationSchema> {
-        return from(this._notificationsService.getUserNotifications(userId))
-            .pipe(
-                tap(msg => this._logger.log(JSON.stringify(msg)))
-            );
+        return this._notificationsService.getUserNotifications(userId);
     }
-
 }
 
 
