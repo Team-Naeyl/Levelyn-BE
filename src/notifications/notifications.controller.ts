5eb79ee0ae271cdea88873bdb3e61664468a6635
hoseok <hs1j333@gmail.com>
2025-07-10 07:48:08 +0900

    commit - 20250710 notification blpop -> lpop + setTimeout

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Inject, Sse, UseGuards, UseInterceptors } from '@nestjs/common';
 import { SseJwtAuthGuard } from "../auth";
-import { AuthQuerySchema, SseInterceptor, User } from "../common";
-import { from, interval, map, merge, Observable } from "rxjs";
+import { AuthQuerySchema, SseInterceptor, User, UserEvent } from "../common";
+import { from, fromEvent, interval, map, merge, Observable, tap } from "rxjs";
 import { ApiOkResponse, ApiOperation, ApiQuery, ApiTags } from "@nestjs/swagger";
 import { NotificationSchema } from "./api";
 import { NotificationsService } from "./notifications.service";
+import { EventEmitter2 } from "@nestjs/event-emitter";
 
 @ApiTags("Notifications")
 @Controller('/api/notifications')
@@ -15,6 +16,8 @@ export class NotificationsController {
     constructor(
         @Inject(NotificationsService)
         private readonly _notificationsService: NotificationsService,
+        @Inject(EventEmitter2)
+        private readonly _eventEmitter: EventEmitter2
     ) {}
 
     @Sse("/")
@@ -22,12 +25,12 @@ export class NotificationsController {
     @ApiQuery({ type: AuthQuerySchema, required: true })
     @ApiOkResponse({ type: NotificationSchema })
     notifyUser(@User("id") userId: number): Observable<NotificationSchema> {
-        return merge(
-            interval(15000).pipe(
-                map(() => ({ event: "ping", data: null }))
-            ),
-            this._notificationsService.getUserNotifications(userId)
-        );
+        return fromEvent(this._eventEmitter, `user.${userId}.event`)
+            .pipe(
+                tap(msg => JSON.stringify(msg)),
+                map(msg => msg as UserEvent),
+                map(({ userId, ...rest }) => rest)
+            );
     }
 }
 
